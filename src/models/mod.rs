c05ca743fd16fa4ab68280a8f7ab19d2ca494c56
oussemamaknii <oussema.makni@esprit.tn>
2023-03-30 12:43:47 +0200

    fixed null fields issues (so happy)

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -112,128 +112,358 @@ impl<'a> IntoIterator for &'a Fields {
     }
 }
 
+trait ShouldSkip {
+    fn should_skip(&self) -> bool;
+}
+
+impl<T: Default + PartialEq> ShouldSkip for Option<T> {
+    fn should_skip(&self) -> bool {
+        self.as_ref().map(|v| *v == T::default()).unwrap_or(true)
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all(serialize = "lowercase", deserialize = "UPPERCASE"))]
+#[serde(rename_all(serialize = "UPPERCASE", deserialize = "UPPERCASE"))]
 pub struct Fields {
     #[serde(skip_serializing)]
     #[serde(default)]
     pub map: HashMap<String, Option<Base64>>,
     //---------------Generic Data-------------------------
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gprmc_valid: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_speed: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_dir: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_altitude: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub dio_ignition: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub batt: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gprs_header: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub rssi: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_ext_batt_present: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub odo_partial_km: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub odo_full: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub dio_alarm: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub driver_id: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub dio_in_tor: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_hdop: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_pdop: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_vdop: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub batt_temp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub case_temp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub modem_network_operator: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_average_pdop_status: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_last_valid_gps_latitude: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_last_valid_gps_longitude: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub area_list: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub gps_fixed_sat_num: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mvt_state: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub boot_reason: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub shutdown_type_and_reason: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub batt_volt: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub tx_kbytes: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub rx_kbytes: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub batt_warmup: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub batt_charging: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub dio_out_tor: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub modem_sim_iccid: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub modem_sim_imsi: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub serial_ppp_state: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub board_id: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub event: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_ext_batt_low: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_ext_batt_voltage: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub odo_partial_meter: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub odo_full_meter: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_zone_country: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_zone_state: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_vehicle_state_mvt: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_gps_antenna: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_dio_ain1: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_fuel_type: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_timezone: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_night_and_day: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_stat_global_trip_distance: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_stat_global_trip_fuel_consumed: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_serial_number: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_software_version: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_unplug_duration: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_unplug_timestamp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_unplug_sporadic: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_replug_timestamp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_unplug_count: Option<Base64>,
     //---------------OBD Data-------------------------
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub obd_connected_protocol: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub obd_fuel_level_ratio: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_fuel_level: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_engine_load: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_fuel_pressure: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_throttle_position: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_engine_oil_temp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_transmission_oil_temp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_engine_oil_pressure: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_cruise_control: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_state_water_in_fuel: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_engine_coolant_level: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_engine_coolant_temp: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_engine_coolant_pressure: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_fuel_delivery_pressure: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_parking_brake_switch: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_brake_application_pressure: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_brake_pedal_status: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_stack_name: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_brake_pedal_position: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_vin_alt: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_fuel_type: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_monitor_status: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_vin_hash: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_pid_1: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_pid_2: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_pid_3: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_pid_4: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_pid_5: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_squish_vin: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_mileage_meters: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_speed: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_rpm: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_fuel: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_vin: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_mileage: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_engine_mode: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_engine_combustion_mode_time: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_engine_electric_mode_time: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_charging_state: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_battery_voltage: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_battery_current: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_battery_state_of_charge: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_hev_state_supported: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_co2_emissions: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub obd_supported_pids_00: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_distance_since_dtc_cleared: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_time_since_dtc_cleared: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_ext_volt_snapshot: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_ambient_air_temperature: Option<Base64>,
+
+    #[serde(skip_serializing_if = "ShouldSkip::should_skip")]
     pub mdi_obd_barometric_pressure: Option<Base64>,
 }
 
-#[derive(Debug, Serialize, Deserialize, IsEmpty, Clone)]
+#[derive(Debug, Serialize, Default, PartialEq, Deserialize, IsEmpty, Clone)]
 pub struct Base64 {
     #[serde(skip_serializing_if = "Option::is_none")]
     b64_value: Option<String>,
